9f16e9722de10190b9ef1b8b7e6d422c079a19d8
Brian McKenna <brianmckennaman@gmail.com>
2022-09-20 17:42:20 -0400

    added helper directory and updated server.js file

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const notes = require('express').Router();
+const { readAndAppend } = require('./helpers/fsUtils');
 const path = require('path');
 
 const app = express();
@@ -7,3 +9,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.port || 3001;
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/notes.html'))
+    );
+
+notes.get('/', (req, res) => readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data))));
+
+notes.post('/', (req, res) => {
+    const { title, text } = req.body;
+
+    if(title && text) {
+        const newNote = {
+            title,
+            text
+        };
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting new note')
+    }
+})
+
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`))
