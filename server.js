a2599dba50aa359676a7545e16f1ad9a7b09417d
Brian McKenna <brianmckennaman@gmail.com>
2022-09-19 21:16:09 -0400

    added server.js file

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.port || 3001;
