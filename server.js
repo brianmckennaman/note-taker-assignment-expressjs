4f0e645ef0dcceffb152e2aec07c4c5319a32117
Brian McKenna <brianmckennaman@gmail.com>
2022-09-21 18:11:58 -0400

    adjusted heroku deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
